adef8c50fbddc4810f917c2ea400dbe8debe7d59
Knight1001 <jianyishi1995@outlook.com>
2019-03-09 18:06:09 -0500

    test on windows

---
+++ project1-a taste of cpp/cmake-build-debug/CMakeFiles/project1_a_taste_of_cpp.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Chris S/Documents/Intro-to-Cpp/project1-a taste of cpp/lib/StanfordCPPLib/spl.cpp" "C:/Users/Chris S/Documents/Intro-to-Cpp/project1-a taste of cpp/cmake-build-debug/CMakeFiles/project1_a_taste_of_cpp.dir/lib/StanfordCPPLib/spl.cpp.obj"
+  "C:/Users/Chris S/Documents/Intro-to-Cpp/project1-a taste of cpp/src/name_hash.cpp" "C:/Users/Chris S/Documents/Intro-to-Cpp/project1-a taste of cpp/cmake-build-debug/CMakeFiles/project1_a_taste_of_cpp.dir/src/name_hash.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib/StanfordCPPLib/collections"
+  "../lib/StanfordCPPLib/graphics"
+  "../lib/StanfordCPPLib/io"
+  "../lib/StanfordCPPLib/private"
+  "../lib/StanfordCPPLib/system"
+  "../lib/StanfordCPPLib/util"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
